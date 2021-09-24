a990c576beda93395c7191dd35dd490af793e577
iyukukuku <nurruddinafif16@github.com>
2021-09-24 14:25:57 +0700

    Make Contact

--- tugas_prak_1/lib/main.dart
+++ tugas_prak_1/lib/main.dart
@@ -5,45 +5,46 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  List<String> List_NamaPemain = [
-    "Beckam",
-    "C.Ronaldo",
-    "Messi",
-    "Neymar",
-    "Paquito",
-    "Estes",
+  //const MyApp({Key? key}) : super(key: key);
+
+  List<String> list_Nama = [
+    "Sulthon Wagyu Alima",
+    "Amir Saiful Dian",
+    "Daud Dwi Setiawan",
+    "Danial Ibrahim Krisna inyrarigrapari",
+    "Lutfi Harun Asih",
+    "Vina Zulfikar Ratu",
     "Nana"
   ];
-  List<String> List_Negara = [
-    "Gatau",
-    "Portugal",
-    "Argentina",
-    "Brazil",
-    "Omahmu",
-    "Land Of Dawn",
-    "Deketnya Paquito"
-  ];
-  List<String> List_Club = [
-    "Arsenal",
-    "MU",
-    "Mantan Barca",
-    "PSG",
-    "Fighter",
-    "Support",
-    "Apakah Kamu ingin.."
-  ];
 
-  //const MyApp({Key? key}) : super(key: key);
+  List<String> list_Email = [
+    "sulthon.wagyuA5@yahoo.com",
+    "amir.saiful@yahoo.com",
+    "daud55@yahoo.com",
+    "graparinyariin@yahoo.com",
+    "luthfiharun@yahoo.com",
+    "vinaRatu@yahoo.com",
+    "clifton.nana@hotmail.com"
+  ];
+  List<String> list_NoTelpon = [
+    "0860-3070-6405",
+    "0867-439-523",
+    "0870-7039-94213",
+    "0837-1620-97591",
+    "0860-5032-9699",
+    "0883-4807-876",
+    "0880-8993-5927"
+  ];
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("List Pemain Baru 2021"),
+          title: Text("Daftar Kontak Redmi Galaxy XR"),
         ),
         body: ListView(children: [
-          for (int i = 0; i < List_NamaPemain.length; i++)
+          for (int i = 0; i < list_Nama.length; i++)
             Container(
               width: double.infinity,
               height: 150,
@@ -51,7 +52,7 @@ class MyApp extends StatelessWidget {
               padding:
                   EdgeInsets.only(left: 30, top: 10, right: 10, bottom: 10),
               decoration: BoxDecoration(
-                  color: Colors.orange,
+                  color: Colors.yellow,
                   borderRadius: BorderRadius.circular(10)),
               child: Row(
                 children: [
@@ -59,26 +60,17 @@ class MyApp extends StatelessWidget {
                   Flexible(
                       fit: FlexFit.tight,
                       child: Container(
-                        margin: EdgeInsets.only(left: 30, top: 30),
+                        margin: EdgeInsets.only(left: 25, top: 40),
                         height: 130,
                         width: 150,
-                        color: Colors.orange,
+                        color: Colors.yellow,
                         child: Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           mainAxisAlignment: MainAxisAlignment.start,
                           children: [
-                            Text(List_NamaPemain[i]),
-                            Text(List_Negara[i]),
-                            Text(List_Club[i]),
-                            Row(
-                              children: [
-                                for (int i = 0; i < 5; i++)
-                                  Icon(
-                                    Icons.star,
-                                    color: Colors.yellow,
-                                  )
-                              ],
-                            ),
+                            Text(list_Nama[i]),
+                            Text(list_Email[i]),
+                            Text(list_NoTelpon[i]),
                           ],
                         ),
                       ))
